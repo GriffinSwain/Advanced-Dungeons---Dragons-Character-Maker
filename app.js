8f00360725f5a3662ccd3bf6f6856a52c6bfbf84
Griffin <116022056+GriffinSwain@users.noreply.github.com>
2023-04-12 14:38:36 -0700

    Advanced Dungeons & Dragons Character Maker

---
+++ app.js
@@ -0,0 +1,225 @@
+//  4//11/2023      Griffin P
+// I am planning on making an Advanced Dungeons & Dragons character creator
+// The steps I need to code out are
+/*
+   To begin, the user will need to choose their stat rolling method when beginning to create their character.
+   Either roll 3d6, or 4d6 minus the lowest roll six times to determine their stats.
+   Either have them "rolled-down" or have the player choose which stats get which rolls
+   When the stats are determined, the player will choose from the classes/multiclasses that their character can be (based on their stats)
+        > If a character can chose a class, but only play as a certain race (they need a racial stat buff), they can choose that class but only those races
+            > Vice-versa for classes that lose the stats required due to racial stat decreases
+   They will then choose the race that their character is (based on their class)
+   Their stats will be adjusted based on their race
+*/
+
+import {saveToLocalStorage, getLocalStorage, removeFromLocalStorage} from './localStorage.js';
+
+
+let rollButton = document.getElementById("rollButton");
+let rollDownButton = document.getElementById("rollDownButton");
+let statChoiceButton = document.getElementById("statChoiceButton");
+let threeDiceButton = document.getElementById("threeDiceButton");
+let fourDiceButton = document.getElementById("fourDiceButton");
+let continueButton = document.getElementById("continueButton");
+
+let statDisplay = document.getElementById("statDisplay");
+
+let strengthText = document.getElementById("strengthText");
+let dexterityText = document.getElementById("dexterityText");
+let constitutionText = document.getElementById("constitutionText");
+let intelligenceText = document.getElementById("intelligenceText");
+let wisdomText = document.getElementById("wisdomText");
+let charismaText = document.getElementById("charismaText");
+
+let strengthStat = document.getElementById("strengthStat");
+let dexterityStat = document.getElementById("dexterityStat");
+let constitutionStat = document.getElementById("constitutionStat");
+let intelligenceStat = document.getElementById("intelligenceStat");
+let wisdomStat = document.getElementById("wisdomStat");
+let charismaStat = document.getElementById("charismaStat");
+
+let tempStat = [];
+let chooseStats = [];
+let finishedStats = new Array(6);
+let arrayPosition = 0;
+let strength = 0;
+let dexterity = 0;
+let constitution = 0;
+let intelligence = 0;
+let wisdom = 0;
+let charisma = 0;
+let selectedClass = "";
+let rollDown = true;
+let threeDice = true;
+let continueCheck = false;
+
+
+
+rollDownButton.addEventListener('click', function(){
+    rollDown = true;
+    rollDownButton.className = "btn btn-primary";
+    statChoiceButton.className = "btn btn-dark";
+})
+
+statChoiceButton.addEventListener('click', function(){
+    rollDown = false;
+    statChoiceButton.className = "btn btn-success";
+    rollDownButton.className = "btn btn-dark";
+})
+
+threeDiceButton.addEventListener('click', function(){
+    threeDice = true;
+    threeDiceButton.className = "btn btn-danger";
+    fourDiceButton.className = "btn btn-dark";
+})
+
+fourDiceButton.addEventListener('click', function(){
+    threeDice = false;
+    fourDiceButton.className = "btn btn-warning";
+    threeDiceButton.className = "btn btn-dark";   
+})
+
+rollButton.addEventListener('click', function(){
+    strength = 0;
+    dexterity = 0;
+    constitution = 0;
+    intelligence = 0;
+    wisdom = 0;
+    charisma = 0;
+    statDisplay.innerText = "";
+    strengthStat.innerText = "";
+    dexterityStat.innerText = "";
+    constitutionStat.innerText = "";
+    intelligenceStat.innerText = "";
+    wisdomStat.innerText = "";
+    charismaStat.innerText = "";
+    StatTotal();
+    rollButton.textContent = "Reroll Stats";
+})
+
+strengthText.addEventListener('click', function(){
+    if (strength == 0){
+        strengthStat.innerText = chooseStats[arrayPosition];
+        strength = chooseStats[0];
+        finishedStats[0] = strengthStat.textContent;
+        continueCheck = true;
+        FinishCheck()
+    }
+})
+
+
+dexterityText.addEventListener('click', function(){
+    if (dexterity == 0){
+        dexterityStat.innerText = chooseStats[arrayPosition];
+        dexterity = chooseStats[0];
+        FinishCheck()
+    }
+})
+
+constitutionText.addEventListener('click', function(){
+    if (constitution == 0){
+        constitutionStat.innerText = chooseStats[arrayPosition];
+        constitution = chooseStats[0];
+        FinishCheck()
+    }
+})
+
+intelligenceText.addEventListener('click', function(){
+    if (intelligence == 0){
+        intelligenceStat.innerText = chooseStats[arrayPosition];
+        intelligence = chooseStats[0];
+        FinishCheck()
+    }
+})
+
+wisdomText.addEventListener('click', function(){
+    if (wisdom == 0){
+        wisdomStat.innerText = chooseStats[arrayPosition];
+        wisdom = chooseStats[0];
+        FinishCheck()
+    }
+})
+
+charismaText.addEventListener('click', function(){
+    if (charisma == 0){
+        charismaStat.innerText = chooseStats[arrayPosition];
+        charisma = chooseStats[0];
+        console.log(arrayPosition);
+        console.log(chooseStats[arrayPosition])
+        FinishCheck()
+    }
+})
+
+continueButton.addEventListener('click', function(){
+    if (continueCheck){
+        finishedStats[0] = strength;
+        finishedStats[1] = dexterity;
+        finishedStats[2] = constitution;
+        finishedStats[3] = intelligence;
+        finishedStats[4] = wisdom;
+        finishedStats[5] = charisma;
+        console.log(finishedStats);
+        saveToLocalStorage(finishedStats);
+        window.location = '/Pages/classChoice.html';
+    } 
+})
+
+function FinishCheck(){
+        chooseStats.shift();
+        console.log (chooseStats);
+        statDisplay.innerText = chooseStats;
+        if(strength != 0 && dexterity != 0 && constitution != 0 && intelligence != 0 && wisdom != 0 && charisma != 0){
+            continueButton.className = "btn btn-success";
+            continueCheck = true;
+        } 
+}
+
+function StatRoller(){
+    tempStat = [];
+    let statCounter = 0;
+    if (threeDice){
+        statCounter = (Math.ceil(Math.random(5) * 6)) + (Math.ceil(Math.random(5) * 6)) + (Math.ceil(Math.random(5) * 6))
+    }else{
+        let statArray = [(Math.ceil(Math.random(5) * 6)), (Math.ceil(Math.random(5) * 6)), (Math.ceil(Math.random(5) * 6)) ,(Math.ceil(Math.random(5) * 6))]
+        console.log(statArray);
+        statArray.sort();
+        statArray.shift();
+        statCounter = statArray[0] + statArray[1] + statArray[2];
+        console.log(statArray);
+    }
+    console.log(statCounter);
+   return statCounter;
+}
+
+
+function StatTotal(){
+    let i = 0;
+    let tempStat = [];
+    statDisplay.innerText = "";
+    if (rollDown){
+        strengthStat.innerText = StatRoller();
+        dexterityStat.innerText = StatRoller();
+        constitutionStat.innerText = StatRoller();
+        intelligenceStat.innerText = StatRoller();
+        wisdomStat.innerText = StatRoller();
+        charismaStat.innerText = StatRoller();
+        continueButton.className = "btn btn-success";
+        strength = strengthStat.textContent;
+        dexterity = dexterityStat.textContent;
+        constitution = constitutionStat.textContent;
+        intelligence = intelligenceStat.textContent;
+        wisdom = wisdomStat.textContent;
+        charisma = charismaStat.textContent;
+        continueCheck = true;
+    }else{
+        while (i < 6){
+            tempStat.push(StatRoller());
+            i++;
+        }
+        continueButton.className = "btn btn-dark"
+        continueCheck = false;
+        statDisplay.innerText = tempStat;
+        chooseStats = tempStat;
+        console.log(chooseStats);
+    }
+}
---
+++ classRace.js
@@ -0,0 +1,357 @@
+import { saveToLocalStorage, getLocalStorage, removeFromLocalStorage } from './localStorage.js';
+
+let data;
+data = getLocalStorage();
+let choice = data.length - 1;
+
+let backButton = document.getElementById("backButton");
+
+let injectHere = document.getElementById("injectHere");
+let injectHere2 = document.getElementById("injectHere2");
+
+let selectedClass = document.getElementById("selectedClass");
+let selectedRace = document.getElementById("selectedRace");
+let strengthStat = document.getElementById("strengthStat");
+let dexterityStat = document.getElementById("dexterityStat");
+let constitutionStat = document.getElementById("constitutionStat");
+let intelligenceStat = document.getElementById("intelligenceStat");
+let wisdomStat = document.getElementById("wisdomStat");
+let charismaStat = document.getElementById("charismaStat");
+
+let characterClass = "";
+let characterRace = "";
+
+let strength = 0;
+let dexterity = 0;
+let constitution = 0;
+let intelligence = 0;
+let wisdom = 0;
+let charisma = 0;
+
+let racialStatChange = false;
+let lastRace = "";
+
+let fighter = false;
+let paladin = false;
+let ranger = false;
+let mage = false;
+let specalist = false;
+let cleric = false;
+let druid = false;
+let thief = false;
+let bard = false;
+
+let dwarf = false;
+let elf = false;
+let gnome = false;
+let halfelf = false;
+let halfing = false;
+
+StatGetter();
+ClassDisplay();
+
+backButton.addEventListener('click', function(){
+    window.location = '/index.html';
+})
+
+function StatGetter() {
+    strength = data[choice][0];
+    dexterity = data[choice][1];
+    constitution = data[choice][2];
+    intelligence = data[choice][3];
+    wisdom = data[choice][4];
+    charisma = data[choice][5];
+    strengthStat.textContent = strength;
+    dexterityStat.textContent = dexterity;
+    constitutionStat.textContent = constitution;
+    intelligenceStat.textContent = intelligence;
+    wisdomStat.textContent = wisdom;
+    charismaStat.textContent = charisma;
+}
+
+function ClassDisplay() {
+    if (strength >= 9) fighter = true;
+    if (strength >= 12 && constitution >= 9 && wisdom >= 13 && charisma >= 17) paladin = true;
+    if (strength >= 9 && dexterity >= 13 && constitution >= 14 && wisdom >= 14) ranger = true;
+    if (intelligence >= 9) mage = true;
+    if (wisdom >= 9) cleric = true;
+    if (wisdom >= 12 && charisma >= 15) druid = true;
+    if (dexterity >= 9) thief = true;
+    if (dexterity >= 12 && intelligence >= 13 && charisma >= 15) bard = true;
+
+    let fighterBtn = document.createElement("Button");
+    fighterBtn.innerText = "Fighter";
+    fighterBtn.className = "btn btn-danger";
+    fighterBtn.addEventListener("click", function () {
+        characterClass = "fighter";
+        selectedClass.innerText = "Fighter";
+        AppendClass();
+        RaceDisplay();
+    })
+    let paladinBtn = document.createElement("Button");
+    paladinBtn.innerText = "Paladin";
+    paladinBtn.className = "btn btn-danger mt-1";
+    paladinBtn.addEventListener("click", function () {
+        characterClass = "paladin";
+        selectedClass.innerText = "Paladin";
+        AppendClass();
+        RaceDisplay();
+    })
+    let rangerBtn = document.createElement("Button");
+    rangerBtn.innerText = "Ranger";
+    rangerBtn.className = "btn btn-danger mt-1";
+    rangerBtn.addEventListener("click", function () {
+        characterClass = "ranger";
+        selectedClass.innerText = "Ranger";
+        AppendClass();
+        RaceDisplay();
+    })
+    let mageBtn = document.createElement("Button");
+    mageBtn.innerText = "Mage";
+    mageBtn.className = "btn btn-primary mt-1";
+    mageBtn.addEventListener("click", function () {
+        characterClass = "mage";
+        selectedClass.innerText = "Mage";
+        AppendClass();
+        RaceDisplay();
+    })
+    let clericBtn = document.createElement("Button");
+    clericBtn.innerText = "Cleric";
+    clericBtn.className = "btn btn-success mt-1";
+    clericBtn.addEventListener("click", function () {
+        characterClass = "cleric";
+        selectedClass.innerText = "Cleric";
+        AppendClass();
+        RaceDisplay();
+    })
+    let druidBtn = document.createElement("Button");
+    druidBtn.innerText = "Druid";
+    druidBtn.className = "btn btn-success mt-1";
+    druidBtn.addEventListener("click", function () {
+        characterClass = "druid";
+        selectedClass.innerText = "Druid";
+        AppendClass();
+        RaceDisplay();
+    })
+    let thiefBtn = document.createElement("Button");
+    thiefBtn.innerText = "Thief";
+    thiefBtn.className = "btn btn-secondary mt-1";
+    thiefBtn.addEventListener("click", function () {
+        characterClass = "thief";
+        selectedClass.innerText = "Thief";
+        AppendClass();
+        RaceDisplay();
+    })
+    let bardBtn = document.createElement("Button");
+    bardBtn.innerText = "Bard";
+    bardBtn.className = "btn btn-secondary mt-1";
+    bardBtn.addEventListener("click", function () {
+
+        characterClass = "bard";
+        selectedClass.innerText = "Bard";
+        AppendClass();
+        RaceDisplay();
+    })
+
+    AppendClass();
+
+    function AppendClass() {
+
+        injectHere.innerHTML = "";
+        characterRace = "";
+        selectedRace.innerText = "";
+        RacialStats();
+        lastRace = "";
+        
+        if (fighter) injectHere.appendChild(fighterBtn);
+        if (paladin) injectHere.appendChild(paladinBtn);
+        if (ranger) injectHere.appendChild(rangerBtn);
+        if (mage) injectHere.appendChild(mageBtn);
+        if (cleric) injectHere.appendChild(clericBtn);
+        if (druid) injectHere.appendChild(druidBtn);
+        if (thief) injectHere.appendChild(thiefBtn);
+        if (bard) injectHere.appendChild(bardBtn);
+
+    }
+}
+
+
+function RaceDisplay() {
+
+    injectHere2.innerHTML = "";
+
+    switch (characterClass) {
+        case "fighter":
+            dwarf = true;
+            elf = true;
+            gnome = true;
+            halfelf = true;
+            halfing = true;
+            break;
+        case "paladin":
+            dwarf = false;
+            elf = false;
+            gnome = false;
+            halfelf = false;
+            halfing = false;
+            break;
+        case "ranger":
+            dwarf = false;
+            elf = true;
+            gnome = false;
+            halfelf = true;
+            halfing = false;
+            break;
+        case "mage":
+            dwarf = false;
+            elf = true;
+            gnome = false;
+            halfelf = true;
+            halfing = false;
+            break;
+        case "cleric":
+            dwarf = true;
+            elf = true;
+            gnome = true;
+            halfelf = true;
+            halfing = true;
+            break;
+        case "druid":
+            dwarf = false;
+            elf = false;
+            gnome = false;
+            halfelf = true;
+            halfing = false;
+            break;
+        case "thief":
+            dwarf = true;
+            elf = true;
+            gnome = true;
+            halfelf = true;
+            halfing = true;
+            break;
+        case "bard":
+            dwarf = false;
+            elf = false;
+            gnome = false;
+            halfelf = true;
+            halfing = false;
+            break;
+
+    }
+
+    let dwarfBtn = document.createElement("Button");
+    dwarfBtn.innerText = "Dwarf";
+    dwarfBtn.className = "btn btn-secondary mt-1";
+    dwarfBtn.addEventListener("click", function () {
+
+        characterRace = "dwarf";
+        selectedRace.innerText = "Dwarf"
+        RacialStats();
+        lastRace = "dwarf";
+    })
+    let elfBtn = document.createElement("Button");
+    elfBtn.innerText = "Elf";
+    elfBtn.className = "btn btn-success mt-1";
+    elfBtn.addEventListener("click", function () {
+        characterRace = "elf";
+        selectedRace.innerText = "Elf"
+        RacialStats();
+        lastRace = "elf";
+    })
+    let gnomeBtn = document.createElement("Button");
+    gnomeBtn.innerText = "Gnome";
+    gnomeBtn.className = "btn btn-info mt-1";
+    gnomeBtn.addEventListener("click", function () {
+        characterRace = "gnome";
+        selectedRace.innerText = "Gnome"
+        RacialStats();
+        lastRace = "gnome";
+    })
+    let halfelfBtn = document.createElement("Button");
+    halfelfBtn.innerText = "Half-elf";
+    halfelfBtn.className = "btn btn-primary mt-1";
+    halfelfBtn.addEventListener("click", function () {
+        characterRace = "halfelf";
+        selectedRace.innerText = "Half-Elf"
+    })
+    let halflingBtn = document.createElement("Button");
+    halflingBtn.innerText = "Halfling";
+    halflingBtn.className = "btn btn-warning mt-1";
+    halflingBtn.addEventListener("click", function () {
+        characterRace = "halfling";
+        RacialStats();
+        selectedRace.innerText = "Halfling";
+        lastRace = "halfling";
+    })
+    let humanBtn = document.createElement("Button");
+    humanBtn.innerText = "Human";
+    humanBtn.className = "btn btn-danger mt-1";
+    humanBtn.addEventListener("click", function () {
+        characterRace = "human";
+        selectedRace.innerText = "Human";
+    })
+
+    if (dwarf) injectHere2.appendChild(dwarfBtn);
+    if (elf) injectHere2.appendChild(elfBtn);
+    if (gnome) injectHere2.appendChild(gnomeBtn);
+    if (halfelf) injectHere2.appendChild(halfelfBtn);
+    if (halfing) injectHere2.appendChild(halflingBtn);
+    injectHere2.appendChild(humanBtn);
+
+}
+
+function RacialStats() {
+
+        switch (characterRace) {
+            case "dwarf":
+                console.log("Charsima down");
+                constitution++;
+                charisma--;
+                break;
+            case "elf":
+                dexterity++;
+                constitution--;
+                break;
+            case "gnome":
+                intelligence++;
+                wisdom--;
+                break;
+            case "halfling":
+                dexterity++;
+                strength--;
+                break;
+            default:
+                break;
+        }
+
+        switch (lastRace) {
+            case "dwarf":
+                console.log("Charsima up");
+                constitution--;
+                charisma++;
+                break;
+            case "elf":
+                dexterity--;
+                constitution++;
+                break;
+            case "gnome":
+                intelligence--;
+                wisdom++;
+                break;
+            case "halfling":
+                dexterity--;
+                strength++;
+                break;
+        }
+    
+    racialStatChange = !racialStatChange;
+
+    strengthStat.textContent = strength;
+    dexterityStat.textContent = dexterity;
+    constitutionStat.textContent = constitution;
+    intelligenceStat.textContent = intelligence;
+    wisdomStat.textContent = wisdom;
+    charismaStat.textContent = charisma;
+
+}
